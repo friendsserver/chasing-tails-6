88a495a006dc66851e72b6923f417d539b259aff
friendsserver <gyeonghwan0419@gmail.com>
2026-02-06 21:31:08 +0900

    Create ChasingtailsConsts.kt

---
+++ src/main/kotlin/me/prdis/chasingtails/plugin/objects/ChasingtailsConsts.kt
@@ -0,0 +1,5 @@
+package me.prdis.chasingtails.plugin.objects
+enum class TeamColor(val chatColor: String, val displayName: String) {
+    RED("§c", "빨강"), ORANGE("§6", "주황"), YELLOW("§e", "노랑"),
+    GREEN("§a", "초록"), BLUE("§9", "파랑"), PURPLE("§5", "보라")
+}
